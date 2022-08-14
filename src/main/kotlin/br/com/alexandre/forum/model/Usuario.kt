856473bc09cabea75a3c5451e62796ae3830502c
Alexandre Queiroz <alexandrepontesgyn@gmail.com>
2022-08-14 18:54:38 -0300

    adiciona migrations

--- src/main/kotlin/br/com/alexandre/forum/model/Usuario.kt
+++ src/main/kotlin/br/com/alexandre/forum/model/Usuario.kt
@@ -9,7 +9,7 @@ import javax.persistence.Id
 data class Usuario (
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long? = null,
+        val id: Long?,
         val nome: String,
         val email: String
 )
