4dd15d9bb9b8a95dfd01c4f40328474cebd20766
Alexandre Queiroz <alexandrepontesgyn@gmail.com>
2022-08-21 17:14:56 -0300

    criando filtro jwt

--- src/main/kotlin/br/com/alexandre/forum/config/SecurityConfig.kt
+++ src/main/kotlin/br/com/alexandre/forum/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package br.com.alexandre.forum.config
 
+import br.com.alexandre.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -9,18 +10,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-    private val userDetailsService: UserDetailsService
+    private val userDetailsService: UserDetailsService,
+    private val jwtUtil: JWTUtil
 ): WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity?) {
         http?.
-        authorizeRequests()?.antMatchers("/topicos")?.hasAuthority("LEITURA-ESCRITA")?.
+        authorizeRequests()?.
+        //antMatchers("/topicos")?.hasAuthority("LEITURA-ESCRITA")?.
+        antMatchers("/login")?.permitAll()?.
         anyRequest()
         ?.authenticated()?.and()
-        ?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
+        http?.addFilterBefore(JWTLoginFilter(authManager = authenticationManager(), jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
          and()?.
          formLogin()?.disable()?.
             httpBasic()
